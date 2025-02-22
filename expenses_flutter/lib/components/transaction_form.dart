07cfcce495937b34a2e64cc3b7a9e7279626cb09
navas <gabrielnavas@protonmail.com>
2025-02-22 08:48:15 -0300

    onsubmit press and verify data

--- expenses_flutter/lib/components/transaction_form.dart
+++ expenses_flutter/lib/components/transaction_form.dart
@@ -8,6 +8,15 @@ class TransactionForm extends StatelessWidget {
 
   TransactionForm(this.onSubmit, {super.key});
 
+  void _submitForm() {
+    final title = titleController.text;
+    final value = double.tryParse(valueController.text) ?? 0.0;
+    if (title.isEmpty || value <= 0) {
+      return;
+    }
+    onSubmit(title, value);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -20,22 +29,20 @@ class TransactionForm extends StatelessWidget {
               decoration: InputDecoration(labelText: 'Título'),
               controller: titleController,
               keyboardType: TextInputType.text,
+              onSubmitted: (_) => _submitForm(),
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Valor (R\$)'),
               controller: valueController,
-              keyboardType: TextInputType.number,
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              onSubmitted: (_) => _submitForm(),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(elevation: 0),
-                  onPressed: () {
-                    final title = titleController.text;
-                    final value = double.tryParse(valueController.text) ?? 0.0;
-                    onSubmit(title, value);
-                  },
+                  onPressed: _submitForm,
                   child: Text(
                     'Nova Transação',
                     style: TextStyle(color: Colors.purple.shade500),
