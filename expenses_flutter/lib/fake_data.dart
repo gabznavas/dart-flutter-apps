7034d6df5bb953526d9c0aec2df704dc5495c526
navas <gabrielnavas@protonmail.com>
2025-02-22 09:41:52 -0300

    separate fake data

---
+++ expenses_flutter/lib/fake_data.dart
@@ -0,0 +1,15 @@
+import 'package:expenses_flutter/models/transaction.dart';
+
+final fakeTransactionsData = [
+  Transaction(id: 't1', title: 'Sorvete', value: 10, date: DateTime.now()),
+  Transaction(id: 't2', title: 'Conta #1', value: 150.00, date: DateTime.now()),
+  Transaction(id: 't3', title: 'Conta #2', value: 211.22, date: DateTime.now()),
+  Transaction(id: 't3', title: 'Conta #3', value: 150.00, date: DateTime.now()),
+  Transaction(id: 't4', title: 'Conta #4', value: 211.22, date: DateTime.now()),
+  Transaction(id: 't5', title: 'Conta #5', value: 150.00, date: DateTime.now()),
+  Transaction(id: 't6', title: 'Conta #6', value: 211.22, date: DateTime.now()),
+  Transaction(id: 't5', title: 'Conta #5', value: 150.00, date: DateTime.now()),
+  Transaction(id: 't6', title: 'Conta #6', value: 211.22, date: DateTime.now()),
+  Transaction(id: 't5', title: 'Conta #5', value: 150.00, date: DateTime.now()),
+  Transaction(id: 't6', title: 'Conta #6', value: 211.22, date: DateTime.now()),
+];
--- expenses_flutter/lib/main.dart
+++ expenses_flutter/lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:expenses_flutter/components/transaction_form.dart';
 import 'package:expenses_flutter/components/transaction_list.dart';
+import 'package:expenses_flutter/fake_data.dart';
 import 'package:expenses_flutter/models/transaction.dart';
 import 'package:flutter/material.dart';
 
@@ -26,69 +27,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<Transaction> _transactions = [
-    Transaction(id: 't1', title: 'Sorvete', value: 10, date: DateTime.now()),
-    Transaction(
-      id: 't2',
-      title: 'Conta #1',
-      value: 150.00,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't3',
-      title: 'Conta #2',
-      value: 211.22,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't3',
-      title: 'Conta #3',
-      value: 150.00,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't4',
-      title: 'Conta #4',
-      value: 211.22,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't5',
-      title: 'Conta #5',
-      value: 150.00,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't6',
-      title: 'Conta #6',
-      value: 211.22,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't5',
-      title: 'Conta #5',
-      value: 150.00,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't6',
-      title: 'Conta #6',
-      value: 211.22,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't5',
-      title: 'Conta #5',
-      value: 150.00,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't6',
-      title: 'Conta #6',
-      value: 211.22,
-      date: DateTime.now(),
-    ),
-  ];
+  final List<Transaction> _transactions = [...fakeTransactionsData];
 
   void _addTransaction(String title, double value) {
     final newId =
