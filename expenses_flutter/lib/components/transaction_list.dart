8535ab38e5efaf42d30faf8e1f664fc81aebcae2
navas <gabrielnavas@protonmail.com>
2025-02-22 22:19:08 -0300

    separate in methods, empty list and render list

--- expenses_flutter/lib/components/transaction_list.dart
+++ expenses_flutter/lib/components/transaction_list.dart
@@ -11,67 +11,70 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const String dateBrazilFormat = 'd MMM y';
-
     return SizedBox(
       height: 400,
       child:
           transactions.isEmpty
-              ? Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Nenhuma Transação cadastrada',
-                    style: Theme.of(context).textTheme.titleMedium,
-                  ),
-                  SizedBox(height: 20),
-                  SizedBox(
-                    height: 200,
-                    child: Image.asset(
-                      'assets/images/waiting.png',
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ],
-              )
-              : ListView.builder(
-                itemCount: transactions.length,
-                itemBuilder: (ctx, index) {
-                  final transaction = transactions[index];
-                  return Card(
-                    margin: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
-                    elevation: 5,
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        radius: 30,
-                        child: Padding(
-                          padding: const EdgeInsets.all(6.0),
-                          child: FittedBox(
-                            child: Text(
-                              'R\$${transaction.value.toStringAsFixed(2)}',
-                            ),
-                          ),
-                        ),
-                      ),
-                      title: Text(
-                        transaction.title,
-                        style: Theme.of(context).textTheme.titleMedium,
-                      ),
-                      subtitle: Text(
-                        DateFormat(dateBrazilFormat).format(transaction.date),
-                        style: Theme.of(context).textTheme.titleSmall,
-                      ),
-                      trailing: IconButton(
-                        onPressed: () => onRemove(transaction.id),
-                        icon: Icon(
-                          Icons.delete,
-                          color: Theme.of(context).colorScheme.error,
-                        ),
-                      ),
-                    ),
-                  );
-                },
+              ? _renderEmptyList(context)
+              : _renderList(context),
+    );
+  }
+
+  Column _renderEmptyList(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Nenhuma Transação cadastrada',
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        SizedBox(height: 20),
+        SizedBox(
+          height: 200,
+          child: Image.asset('assets/images/waiting.png', fit: BoxFit.cover),
+        ),
+      ],
+    );
+  }
+
+  ListView _renderList(BuildContext context) {
+    const String dateBrazilFormat = 'd MMM y';
+
+    return ListView.builder(
+      itemCount: transactions.length,
+      itemBuilder: (ctx, index) {
+        final transaction = transactions[index];
+        return Card(
+          margin: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+          elevation: 5,
+          child: ListTile(
+            leading: CircleAvatar(
+              radius: 30,
+              child: Padding(
+                padding: const EdgeInsets.all(6.0),
+                child: FittedBox(
+                  child: Text('R\$${transaction.value.toStringAsFixed(2)}'),
+                ),
               ),
+            ),
+            title: Text(
+              transaction.title,
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            subtitle: Text(
+              DateFormat(dateBrazilFormat).format(transaction.date),
+              style: Theme.of(context).textTheme.titleSmall,
+            ),
+            trailing: IconButton(
+              onPressed: () => onRemove(transaction.id),
+              icon: Icon(
+                Icons.delete,
+                color: Theme.of(context).colorScheme.error,
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
